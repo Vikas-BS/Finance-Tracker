679c3ce60a664d645a7ad3a3841d1dacbe6602fe
Vikas-BS <vikas.sundaresh@gmail.com>
2025-07-11 16:11:36 +0530

    index

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,6 @@ app.use('/api/income', incomeRoutes);
 app.use('/api/expense', expenseRoutes);
 app.use('/api/home',homeRoutes);
 app.use('/api/user',userRoutes);
-app.options("*", cors());
 app.use('/api/stock',stockRoutes);
 app.get('/' ,(req,res) =>{
   res.json("hello");
