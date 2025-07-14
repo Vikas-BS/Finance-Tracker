8e5ec7ac18a73721db36f9e882e40012104df27c
Vikas-BS <vikas.sundaresh@gmail.com>
2025-07-14 11:16:27 +0530

    home updation

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,47 +1,43 @@
 import React, { useEffect, useState } from "react";
 import DashboardCards from "../components/Dashboardcards";
 
+const Home = () => {
+  const [user, setUser] = useState(null);
 
+  const fetchUser = async () => {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/home`, {
+      method: "GET",
 
-const Home = () =>{
-    const [user , setUser] = useState(null);
+      credentials: "include",
+    });
+    const data = await res.json();
 
-    
-    const fetchUser = async() =>{
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      console.error(data.message);
+    }
+  };
 
-        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/home`, {
-          method: 'GET',
-          
-          credentials: 'include'
-        });
-        const data = await res.json();
-        
-        if(res.ok){
-            setUser(data.user)
-        }else{
-            console.error(data.message)
-        }
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-    };
-    
-    
-    useEffect (()=>{
-        fetchUser();
-    },[]);
-
-
-
-    return (
-      <>
-        {user ? (
-          <div className="text-center min-h-screen w-screen bg-white ">
+  return (
+    <>
+      {user ? (
+        <div className="min-h-screen w-full bg-white dark:bg-slate-950-custom flex items-center justify-center px-4 sm:px-6 md:px-8 py-8">
+          <div className="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl">
             <DashboardCards />
           </div>
-        ) : (
+        </div>
+      ) : (
+        <div className="flex justify-center items-center min-h-screen bg-white dark:bg-slate-950-custom text-gray-700 dark:text-gray-200">
           <p>Loading user info...</p>
-        )}
-      </>
-    );
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
