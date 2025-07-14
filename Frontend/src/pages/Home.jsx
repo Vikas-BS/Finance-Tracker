a98b07a8a92bfc749b2bd36842b3107bb8874e04
Vikas-BS <vikas.sundaresh@gmail.com>
2025-07-14 11:18:44 +0530

    home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -24,19 +24,19 @@ const Home = () => {
   }, []);
 
   return (
-    <>
+    <div className="min-h-screen w-full bg-white  flex items-center justify-center px-4 sm:px-6 md:px-8 py-8">
       {user ? (
-        <div className="min-h-screen w-full bg-white dark:bg-slate-950-custom flex items-center justify-center px-4 sm:px-6 md:px-8 py-8">
+        
           <div className="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl">
             <DashboardCards />
           </div>
-        </div>
+        
       ) : (
         <div className="flex justify-center items-center min-h-screen bg-white dark:bg-slate-950-custom text-gray-700 dark:text-gray-200">
           <p>Loading user info...</p>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
