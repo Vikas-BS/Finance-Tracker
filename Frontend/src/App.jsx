200169732dca3e4747a53013b381b2f9316d88e8
Vikas-BS <vikas.sundaresh@gmail.com>
2025-07-14 11:58:45 +0530

    css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './app.css';
+import './App.css';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
